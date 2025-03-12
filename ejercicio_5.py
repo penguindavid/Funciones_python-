ac9ef94102620ea5cf862bedae60e0981fd63411
penguindavid <davidmacias@colegioguanenta.edu.co>
2025-03-12 07:28:56 -0500

    se actualizo

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,2 +1,35 @@
-# construir una funcion que reciba como parametro una lista de datos numericos y retorne el promedio de los datos pares
+import random
+
+print("-----------------------------------------")
+print("-----MOSTRAR CADENA N VECES EN-----------")
+print("---------------PANTAllA------------------")
+print("-----------------------------------------")
+
+
+def calcular_promedio_pares(lista):
+    suma = 0
+    contador = 0  # Renamed variable from 'count' to 'contador'
+    for i in lista:
+        if i % 2 == 0:
+            suma += i
+            contador += 1
+    if contador == 0:
+        return 0  
+    promedio = suma / contador
+    return promedio
+
+
+# Lista vacía
+lista = []
+
+# Tamaño de la lista
+n = int(input("Digite el tamaño de la lista: "))
+
+for i in range(n):
+    num = random.randint(14, 18)
+    lista.append(num)
+
+# Procesamiento
+print("Lista : ", lista)
+print("El promedio de los datos pares es: ", calcular_promedio_pares(lista))
 
